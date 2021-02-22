062ecf68eac393b9ff75ad0b3f772fbd854dc21b
Edwin Mong'are <37651243+edwinmongare@users.noreply.github.com>
2021-02-22 03:46:22 +0300

    Update publisher.js

--- publisher.js
+++ publisher.js
@@ -7,7 +7,7 @@ const loginFrameReply = new Buffer.from(
   "hex"
 ).toString("ascii");
 const dataframeReply = new Buffer.from(
-  "403A000B1513146916610801FF00260D0A",
+  "403A000B15131469166108010100250D0A",
   "hex"
 ).toString("ascii");
 
